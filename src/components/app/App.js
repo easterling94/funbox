44410b0a2d4e9973be53f01992907751f5870950
Nikita Vinogradov <n.a.vinogradov94@gmail.com>
2023-01-17 23:43:09 +0300

    cards template

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,10 +2,45 @@ import { Card } from '../card/card';
 import styles from './app.module.css'
 
 function App() {
+  const data1 = {
+    textDefault: 'Сказочное заморское яство',
+    title: 'Нямушка',
+    subtitle: 'с фуа-гра',
+    weight: '0,5 кг',
+    description: {
+      portions: 10,
+      gift: 'мышь в подарок',
+    },
+  }
+  const data2 = {
+    textDefault: 'Сказочное заморское яство',
+    title: 'Нямушка',
+    subtitle: 'с рыбой',
+    weight: '2 кг',
+    description: {
+      portions: 40,
+      gift: '2 мыши в подарок',
+    },
+  }
+  const data3 = {
+    textDefault: 'Сказочное заморское яство',
+    title: 'Нямушка',
+    subtitle: 'с курой',
+    weight: '5 кг',
+    description: {
+      portions: 100,
+      gift: '5 мышей в подарок',
+      happy: 'закзчик доволен',
+    },
+  }
   return (
     <div className={styles.app}>
       <h1 className={styles.title}>Ты сегодня покормил кота?</h1>
-      <Card />
+      <section className={styles.cards}>
+        <Card data={data1}/>
+        <Card data={data2}/>
+        <Card data={data3}/>
+      </section>
     </div>
   );
 }
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,8 +1,15 @@
-export const Card = () => {
+import styles from './card.module.css'
+
+export const Card = ({data}) => {
   return (
-    <div>
-      <img src=''></img>
-      <p>Название</p>
+    <div className={styles.card}>
+      <p>{data.textDefault}</p>
+      <h2>{data.title}</h2>
+      <h3>{data.subtitle}</h3>
+      <ul>
+        <li>{data.description.portions} порций</li>
+        <li>{data.description.gift}</li>
+      </ul>
     </div>
   )
 }
